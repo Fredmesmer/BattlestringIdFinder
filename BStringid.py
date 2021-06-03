27f1aed9e2016a43f80ea3e577d5e1ec081c7c43
Fredmesmer <49597918+Fredmesmer@users.noreply.github.com>
2021-06-03 20:19:13 +0200

    Update BStringid.py

--- BStringid.py
+++ BStringid.py
@@ -1,7 +1,32 @@
 import csv
 import time
-from tkmain import window
-from fnvhash import fnv1a_32
+from tkinter import*
+import encodings
+
+class window :
+    def __init__(self,geometry,title):
+        self.core = Tk()
+        self.core.geometry(geometry)
+        self.core.title(title)
+        self.file1 = StringVar(self.core)
+        self.file2 = StringVar(self.core)
+        self.text = StringVar(self.core)
+        self.idd = StringVar(self.core)
+        self.var = StringVar(self.core)
+        self.text.set('idle')
+        self.entitle1 = Label(self.core,text='Reference csv file path').place(x=10,y=5)
+        self.filepath1 = Entry(self.core, textvariable=self.file1,width=40).place(x= 10,y=30)
+        self.entitle2 = Label(self.core,text='Edited csv file path').place(x=10,y=125)
+        self.filepath2 = Entry(self.core, textvariable=self.file2,width=40).place(x=10,y=150)
+        self.process = Button(self.core,text = 'process', command=search).place(x = 300, y = 90)
+        self.output = Label(self.core, textvariable=self.text).place(x = 400,y=95)
+        self.idl = Label(self.core,text = 'id :').place(x=10, y=225)
+        self.idin = Entry(self.core, textvariable=self.idd).place(x=10,y=250)
+        self.iddo = Button(self.core, text = 'hash' ,command = dohashid).place(x = 200,y = 250)
+        self.idre = Entry(self.core,textvariable=self.var).place(x=300,y=250)
+
+def dohashid():
+    screen.var.set(hash(screen.idd.get()))
 
 def hash(stringId) :
     result = 0xFFFFFFFF
@@ -21,62 +46,67 @@ def bruteforce(hexa):
     return str(i)
 
 
+def search():
+    try : 
+        screen.text.set('In progress...')
+        start = time.time()
+        with open(screen.file1.get(), newline='') as f:
+            tabn = ['']*40000
+            i = 0
+            for line in f :
+                if len(line) != 0 :
+                    if i == 0:
+                        for j in range (0,len(str(line)),2):
+                            ording = ord(line[j])
+                            tabn[i] = tabn[i] + chr(ording)
+                    else :
+                        for j in range (1,len(str(line)),2):
+                            ording = ord(line[j])
+                            tabn[i] = tabn[i] + chr(ording)
+                    i += 1
 
-start = time.time()
-
-with open('stringsid.csv', newline='') as f:
-    tabn = ['']*40000
-    i = 0
-    for line in f :
-        if len(line) != 0 :
-            if i == 0:
-                for j in range (0,len(str(line)),2):
-                    ording = ord(line[j])
-                    tabn[i] = tabn[i] + chr(ording)
-            else :
-                for j in range (1,len(str(line)),2):
-                    ording = ord(line[j])
-                    tabn[i] = tabn[i] + chr(ording)
-            i += 1
-
-with open('customnames.csv', newline='') as f:
-    tabe = ['']*40000
-    i = 0
-    for line in f :
-        if line != '' :
-            if i == 0:
-                for j in range (0,len(str(line)),2):
-                    ording = ord(line[j])
-                    tabe[i] = tabe[i] + chr(ording)
-            else :
-                for j in range (1,len(str(line)),2):
-                    ording = ord(line[j])
-                    tabe[i] = tabe[i] + chr(ording)
-            i += 1
+            with open(screen.file2.get(), newline='') as f:
+                tabe = ['']*40000
+                i = 0
+                for line in f :
+                    if line != '' :
+                        if i == 0:
+                            for j in range (0,len(str(line)),2):
+                                ording = ord(line[j])
+                                tabe[i] = tabe[i] + chr(ording)
+                        else :
+                            for j in range (1,len(str(line)),2):
+                                ording = ord(line[j])
+                                tabe[i] = tabe[i] + chr(ording)
+                        i += 1
 
 
-index = 0
+            index = 0
 
-i=0
-j=0
-with open('output.txt','w') as output :
-    while i < len(tabn) and j < len(tabe) :
-        if tabn[i]!='' or tabe[j]!='':
-            if tabn[i][0:8] == tabe[j][0:8]:
-                i += 2
-                j += 2
-            else :
-                print(str(tabe[j]))
-                print('=>'+ bruteforce(tabe[j][0:8]))
-                char = str(tabe[j]) + ' => ' + bruteforce(tabe[j][0:8])+'\n' + '\n'
-                output.write(char)
-                index += 1
-                j += 2
-        else :
-            if tabn[i]=='':
-                i +=1
-            if tabe[j]=='':
-                j +=1
-    output.write('found {} matches.'.format(index))
-    print('found {} matches in {}s.'.format(index,(time.time()-start)))
-    window.change('found {} matches in {}s.'.format(index,(time.time()-start)))
+            i=0
+            j=0
+            with open('output.txt','w') as output :
+                while i < len(tabn) and j < len(tabe) :
+                    if tabn[i]!='' or tabe[j]!='':
+                        if tabn[i][0:8] == tabe[j][0:8]:
+                            i += 2
+                            j += 2
+                        else :
+                            print(str(tabe[j]))
+                            char = str(tabe[j]) 
+                            output.write(char)
+                            index += 1
+                            j += 2
+                    else :
+                        if tabn[i]=='':
+                            i +=1
+                        if tabe[j]=='':
+                            j +=1
+                output.write('found {} matches.'.format(index+1))
+                print('found {} matches in {}s.'.format(index+1,(time.time()-start)))
+                screen.text.set('found {} matches \n in {}s.'.format(index+1,(time.time()-start)))
+    except :
+        screen.text.set('Error try again')
+
+screen = window('500x300','test')
+screen.core.mainloop()
