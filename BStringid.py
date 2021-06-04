559c583573f45c01e4d353472c3fbefd3e5203aa
Fredmesmer <49597918+Fredmesmer@users.noreply.github.com>
2021-06-04 18:56:43 +0200

    Update BStringid.py

--- BStringid.py
+++ BStringid.py
@@ -1,6 +1,7 @@
 import csv
 import time
 from tkinter import*
+from tkinter import filedialog
 import encodings
 
 class window :
@@ -8,22 +9,49 @@ class window :
         self.core = Tk()
         self.core.geometry(geometry)
         self.core.title(title)
+
         self.file1 = StringVar(self.core)
         self.file2 = StringVar(self.core)
+        self.file3 = StringVar(self.core)
         self.text = StringVar(self.core)
         self.idd = StringVar(self.core)
         self.var = StringVar(self.core)
         self.text.set('idle')
+
         self.entitle1 = Label(self.core,text='Reference csv file path').place(x=10,y=5)
-        self.filepath1 = Entry(self.core, textvariable=self.file1,width=40).place(x= 10,y=30)
-        self.entitle2 = Label(self.core,text='Edited csv file path').place(x=10,y=125)
-        self.filepath2 = Entry(self.core, textvariable=self.file2,width=40).place(x=10,y=150)
-        self.process = Button(self.core,text = 'process', command=search).place(x = 300, y = 90)
-        self.output = Label(self.core, textvariable=self.text).place(x = 400,y=95)
+        self.filepath1 = Entry(self.core, textvariable=self.file1,width=50).place(x= 10,y=30)
+        self.pick1 = Button(self.core,text='select file 1',command=pickone).place(x = 320 , y = 25)
+
+        self.entitle2 = Label(self.core,text='Edited csv file path').place(x=10,y=50)
+        self.filepath2 = Entry(self.core, textvariable=self.file2,width=50).place(x=10,y=75)
+        self.pick2 = Button(self.core,text='select file 2',command=picktwo).place(x = 320 , y = 70)
+
+        self.entitle3 = Label(self.core,text='output directory').place(x=10,y=95)
+        self.filepath3 = Entry(self.core, textvariable=self.file3,width=50).place(x=10,y=120)
+        self.pick3 = Button(self.core,text='select output',command=pickthree).place(x = 320 , y = 115)
+
+        self.process = Button(self.core,text = 'process', command=search).place(x = 480, y = 30)
+        self.output = Label(self.core, textvariable=self.text ).place(x=450,y=70)
+
         self.idl = Label(self.core,text = 'id :').place(x=10, y=225)
         self.idin = Entry(self.core, textvariable=self.idd).place(x=10,y=250)
         self.iddo = Button(self.core, text = 'hash' ,command = dohashid).place(x = 200,y = 250)
         self.idre = Entry(self.core,textvariable=self.var).place(x=300,y=250)
+    def browsefile(self):
+        filename = filedialog.askopenfilename(initialdir = "/",title = "Select a File",filetypes = (("csv Files","*.csv*"),("all files","*.*")))
+        return filename
+    def browsedir(self):
+        dirname = filedialog.askdirectory(initialdir = "/",title = "Select a Directory")
+        return dirname
+
+def pickone():
+    screen.file1.set(screen.browsefile())
+
+def picktwo():
+    screen.file2.set(screen.browsefile())
+
+def pickthree():
+    screen.file3.set(screen.browsedir())
 
 def dohashid():
     screen.var.set(hash(screen.idd.get()))
@@ -85,7 +113,9 @@ def search():
 
             i=0
             j=0
-            with open('output.txt','w') as output :
+            a = screen.file3.get()+'/battlestringoutput.txt'
+            print(a)
+            with open(a,'w') as output :
                 while i < len(tabn) and j < len(tabe) :
                     if tabn[i]!='' or tabe[j]!='':
                         if tabn[i][0:8] == tabe[j][0:8]:
@@ -108,5 +138,5 @@ def search():
     except :
         screen.text.set('Error try again')
 
-screen = window('500x300','test')
+screen = window('600x300','test')
 screen.core.mainloop()
